6d0b8835983285058e0dad6c99a01f0b304850d5
Alexandra Bozzini <alexandrabozzini@gmail.com>
2019-07-01 13:21:03 -0400

    Passed tesla tests

--- src/main/java/Vehicles/Tesla.java
+++ src/main/java/Vehicles/Tesla.java
@@ -2,34 +2,50 @@ package Vehicles;
 
 import Driving.Car;
 
+import static oracle.jrockit.jfr.events.Bits.intValue;
+
 public class Tesla extends Car {
+    private Double milesTraveled;
+    private Double topSpeed;
+
+    public Tesla(Double milesTraveled, Double topSpeed) {
+        this.milesTraveled = milesTraveled;
+        this.topSpeed = topSpeed;
+    }
+
+    public Tesla(){
+        this.milesTraveled = 0.0;
+        this.topSpeed = 155.0;
+    }
+
     @Override
     public Boolean needsOilChange() {
-        return null;
+        return false;
     }
 
     @Override
     public void changeOil() {
-
+        // N/A
     }
 
     @Override
     public Boolean checkEngineLight() {
-        return null;
+        return false;
     }
 
     @Override
     public Double getDistanceTraveled() {
-        return null;
+        return this.milesTraveled;
     }
 
     @Override
     public Double getTopSpeed() {
-        return null;
+        return this.topSpeed;
     }
 
     @Override
     public Integer transport(Double distance) {
-        return null;
+        this.milesTraveled += distance;
+        return intValue(distance/this.topSpeed) * 60 * 60;
     }
 }
